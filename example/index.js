a8cb96f9e560ab95d37d6210c1da874ab97393db
Pablo Andres Dorado Suarez <pandres95@boolinc.co>
2015-09-12 03:36:51 -0500

    Good-bye example

--- example/index.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-var booljs = require('..');
-
-// Here is where magic happens
-booljs('com.example.api').run();
--- example/plugins/uuid.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = function (app) {
-    return function () {
-        var uuid = require('node-uuid');
-
-        return uuid.getV1();
-    };
-};
